d4337f6393b82d31c36cd91db65f21a079a2ff15
SergeyDushkin <leirbythe@gmail.com>
2016-02-04 17:37:19 +0300

    Add base info

---
+++ TF/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TF
+{
+    /// <summary>
+    /// Базовый класс "Заказ"
+    /// </summary>
+    public class Order
+    {
+        public DateTime DUEDATE { get; set; }
+        public string CUSTOMER { get; set; }
+        public string DESTINATION { get; set; }
+
+        public IEnumerable<Line> LINES { get; set; }
+
+        /// <summary>
+        /// Базовый класс "Позиция заказа"
+        /// </summary>
+        public class Line
+        {
+            string ITEM { get; set; }
+            public decimal QTY { get; set; }
+            public decimal PRICE { get; set; }
+            public decimal AMOUNT { get; set; }
+        }
+    }
+}
---
+++ TF/OrderController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TF
+{
+    /*
+     * Сценарий:
+     * 1. Сохранение базового класса
+     * 2. Добавление атрибута: Карта лояльности, вариант обработки карты
+     * 3. Изменение логики: 
+     *      для варианта обработки карты лояльности А: скидка 5% на сумму заказа
+     *      для варианта обработки карты лояльности Б: добавить бесплатную колу в заказ
+     * 4. Добавление логики: списание товара со склада
+     */
+
+    /// <summary>
+    /// /api/order/
+    /// Действия с заказом:
+    /// 1.Создать драфт заказа
+    /// Драфт может храниться в памяти
+    /// 2. Изменять драфт заказа
+    /// Отправляется на сервер любое изменение драфта: поля, позиции
+    /// 3. Подтвердить заказ
+    /// </summary>
+    public class OrderController
+    {
+        private readonly OrderApplicationManager app;
+
+        public OrderController()
+        {
+            app = new OrderApplicationManager();
+        }
+
+        /// <summary>
+        /// Work-Flow "Изменение заказа"
+        /// </summary>
+        /// <param name="context"></param>
+        public HttpResponseMessage Patch(System.Web.HttpContext context)
+        {
+            app.UseAuthServer(context);
+        }
+
+        /// <summary>
+        /// Work-Flow "Сохранение заказа"
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Post(System.Web.HttpContext context)
+        {
+            /// Видимо, здесь нужно будет инициировать некую транзакцию
+            /// И навешивать на нее все дальнейшие обработчики
+
+            /// Нужно получить базовую информацию о заказе из request или из хранилища (?)
+            /// Запустить обработку лояльности
+            ///     - будет получена информация о лояльности
+            ///     - изменена базовая информация о заказе (скидка на сумму или добавлен новый товар)
+            ///     - сохранить информацию о лояльности
+            /// Сохранить информацию о заказе
+            /// Списать остатки в WH
+
+            /// Выполнение базового обработчика
+            app.UseDefaultOrderProcess(context);
+
+            /// Обработка карты лояльности
+            app.UseLoyaltyAmountModificatorForOrder(context);
+
+            /// Обработка списаний
+            app.UseWriteOffForOrder(context);
+        }
+
+        /// <summary>
+        /// Получает "заказ"
+        /// </summary>
+        /// <returns></returns>
+        public HttpResponseMessage Get()
+        {
+
+        } 
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OrderApplicationManager
+    {
+        public void UseAuthServer(System.Web.HttpContext context)
+        {
+            /// Auth request
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public void UseDefaultOrderProcess(System.Web.HttpContext context)
+        {
+            /// Get Order from context
+            /// Save Order to DB
+        }
+
+        public void UseLoyaltyAmountModificatorForOrder(System.Web.HttpContext context)
+        {
+            /// Get loyalty number from context
+            /// Get Order from ???
+            /// Modify Order
+            /// Save Loyalty Info
+        }
+
+        public void UseWriteOffForOrder(System.Web.HttpContext context)
+        {
+            /// Get order from ???
+            /// Create WriteOff
+            /// Processed WriteOff
+        }
+    }
+
+    public class LoyaltyCard
+    {
+        public string LOYALTY { get; set; }
+        public string LOYALTY_VARIANT { get; set; }
+    }
+
+    public class AggregateOrder
+    {
+
+    }
+
+
+    public class TransactionController
+    {
+
+    }
+}
