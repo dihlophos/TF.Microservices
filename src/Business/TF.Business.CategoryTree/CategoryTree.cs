760ad6a385f30d6522a6834ed29456292cbd07c5
SergeyDushkin <leirbythe@gmail.com>
2016-02-11 18:16:04 +0300

    Add ProductCategoryService

---
+++ src/Business/TF.Business.CategoryTree/CategoryTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TF.Business
+{
+    /*
+    CREATE TABLE [dbo].[BUSINESS.CATEGORY_TREE](
+	    [GUID_RECORD] [uniqueidentifier] NOT NULL,
+	    [KEY] [nvarchar](50) NOT NULL,
+	    [NAME] [nvarchar](200),
+	    [PARENT_GUID] [uniqueidentifier],
+	    [BATCH_GUID] [uniqueidentifier],
+	    [HIDDEN] [bit] NOT NULL,
+	    [DELETED] [bit] NOT NULL
+    ) ON [PRIMARY]
+    */
+
+    public class CategoryTree
+    {
+        public Guid Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+
+        public Guid ParentId { get; set; }
+    }
+}
---
+++ src/Business/WMS/TF.Business.WMS.ProductCategory/ProductCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TF.Business.WMS
+{
+    /*
+    CREATE TABLE [dbo].[BUSINESS.WMS.PRODUCT_N_SERVICE_CATEGORY](
+	    [GUID_RECORD] [uniqueidentifier] NOT NULL,
+	    [CATEGORY_GUID] [uniqueidentifier] NOT NULL,
+	    [ITEM_GUID] [uniqueidentifier] NOT NULL,
+	    [BATCH_GUID] [uniqueidentifier],
+	    [HIDDEN] [bit] NOT NULL,
+	    [DELETED] [bit] NOT NULL
+    ) ON [PRIMARY]
+    */
+
+    public class ProductCategory
+    {
+        public Guid Id { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Guid ProductId { get; set; }
+    }
+}
