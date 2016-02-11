418ad39854a9e9ce6b58840a3375c45d78949f05
SergeyDushkin <leirbythe@gmail.com>
2016-02-11 19:34:52 +0300

    Price class fix

--- src/Business/WMS/TF.Business.WMS.Price/Price.cs
+++ src/Business/WMS/TF.Business.WMS.Price/Price.cs
@@ -37,6 +37,6 @@ namespace TF.Business.WMS
         /// <summary>
         /// Product price
         /// </summary>
-        public float Price { get; set; }
+        public float ProductPrice { get; set; }
     }
 }
