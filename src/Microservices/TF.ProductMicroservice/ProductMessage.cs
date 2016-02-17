4deaef14d322e87279fe5cd130a3a0c649fe1e03
Alex My <alexmy@outlook.com>
2016-02-17 20:47:36 +0300

    Lost files

---
+++ src/Microservices/TF.ProductMicroservice/ProductMessage.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace TF.ProductMicroservice
+{
+    public class ProductMessage
+    {
+        public ProductMessage()
+        {
+            this.Links = new List<ProductLink>();
+        }
+
+        public ProductMessage(Product product):this()
+        {
+            this.Id = product.Id;
+            this.Type = product.Type;
+            this.Name = product.Name;
+            this.Links.Add(new ProductLink() { Id = this.Id, Name = this.Name, Type = "self" }); // вообще это здесь не нужно скорее всего, оно должно быть на эндпоинте. Но пусть будет пока
+            if (product.Parent!=null)
+            {
+                this.Links.Add(new ProductLink() { Id = product.Parent.Id, Name = product.Parent.Name, Type = "parent" });
+            }
+
+            foreach (var child in product.Childs)
+            {
+                this.Links.Add(new ProductLink() { Type = child.Type, Name = child.Name, Id = child.Id });
+            }
+        }
+
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "links")]
+        public List<ProductLink> Links { get; set; }
+    }
+}
---
+++ src/Microservices/TF.ProductMicroservice/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TF.ProductMicroservice
+{
+    public class ProductsController : ApiController
+    {
+        IProductRepository _repository;
+
+        public ProductsController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IHttpActionResult Get() //все списал у Сереги. Нужно больше проверок...
+        {
+            IEnumerable<Product> products = _repository.Get();
+            return Ok(Product.CreateMessages(products));
+        }
+
+        public IHttpActionResult Get(Guid id)
+        {
+            var product = _repository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product.GetMessage());
+        }
+
+        public IHttpActionResult Post([FromBody]Product product)
+        {
+            return Ok(_repository.Save(product));
+        }
+
+        public IHttpActionResult Put(Guid id, [FromBody]Product product)
+        {
+           return  Ok(_repository.Save(product));
+        }
+
+        public IHttpActionResult Delete(Guid id)
+        {
+            try
+            {
+                _repository.Delete(id);
+            }
+            catch (ArgumentException)
+            {
+                return NotFound();
+            }
+            return Ok();
+        } 
+    }
+}
